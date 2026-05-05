4faedbdc7092062640e4407600fdaa381f8f1280
William Bach <williambach@Williams-MacBook-Air.local>
2026-05-05 17:21:43 +0200

    Cle API cachee

--- IA_prompts.py
+++ IA_prompts.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
+import os
 
-client = OpenAI(api_key="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")
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 # Entrer un prompt et recuperer son output
 def prompt_IA(system_prompt, user_prompt):
