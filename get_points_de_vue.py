2ebee9952309735ca851bc9e3a28e7a7b501b84a
William Bach <bachwilliam17@gmail.com>
2026-05-23 01:21:07 +0200

    v1 fetch posts X API

---
+++ get_points_de_vue.py
@@ -0,0 +1,72 @@
+import sys
+sys.path.append("/Users/williambach/Documents/Python Library/LLM Requests")
+from IA_prompts import prompt_IA
+
+
+"""
+    Recuperer les descriptions des articles a partir de leur ID 
+    sous forme de texte
+"""
+def get_desc_sujet_string(id_articles, desc_articles):
+    desc_sujet = ""
+
+    # Parcourir chaque article
+    for article in id_articles:
+        # Rajouter la description de l'article actuel
+        desc = desc_articles.get(article['id_article'])
+        if desc is not None:
+            desc_sujet += f"\nArticle: "
+            desc_sujet += f"{desc}."
+
+    return desc_sujet
+
+
+"""
+    Recuperer le point de vue d'un bord politique grace a une requete LLM
+"""
+def get_pdv(desc_sujet, prompt_synthese):
+        # Recuperer le point de vue general avec une requete LLM
+        if "Article" in desc_sujet:
+            return prompt_IA(
+                "gpt-5.4", 
+                prompt_synthese, 
+                desc_sujet
+            )
+        else:
+            return None
+
+
+"""
+    Generer une synthese des points de vue de chaque bord politique 
+    pour tous les sujets d'actualite
+"""
+def get_points_de_vue(sujets, desc_articles, prompt_synthese):
+    print("\nRecuperation de la synthese des points de vue pour chaque camp ..")
+    points_de_vue = {}
+
+    # Parcourir les sujets d'actualite
+    for sujet in sujets['sujets']:
+
+        # Verifier qu'il y a au moins 6 descriptions d'articles
+        articles_count = len(sujet['droite']) + len(sujet['gauche'])
+        if articles_count > 5:
+            # Construire un input avec les descriptions d'articles
+            desc_articles_string = "## DONNÉES D'ENTRÉE"
+
+            desc_articles_string += "\n\nDROITE"
+            desc_articles_string += get_desc_sujet_string(sujet['droite'], desc_articles)
+
+            desc_articles_string += "\n\nGAUCHE"
+            desc_articles_string += get_desc_sujet_string(sujet['gauche'], desc_articles)
+
+            # Recuperer le sujet actuel
+            nom_sujet = sujet['nom_sujet']
+            points_de_vue[nom_sujet] = {}
+
+            # Recuperer les points de vue
+            pdv = get_pdv(desc_articles_string, prompt_synthese)
+            points_de_vue[nom_sujet] = pdv
+            
+    
+    print("\nPoints de vue pour chaque camp recuperes.")
+    return points_de_vue
---
+++ main.py
@@ -0,0 +1,50 @@
+import sys
+sys.path.append("/Users/williambach/Documents/Python Library/LLM Requests")
+
+from IA_prompts import prompt_IA
+from get_articles import get_articles, get_description_articles, get_articles_string
+from get_points_de_vue import get_points_de_vue
+
+import json
+
+# Recuperer tous les articles
+medias_data_json = "flux_rss_medias.json"
+articles = get_articles(medias_data_json)
+
+# Enregistrer les articles dans un fichier
+with open("outputs/description_articles.txt", 'w') as f:
+    for articles_dict in articles['medias']:
+        f.write(f"\n\n\n{articles_dict['journal']}")
+        f.write(f"\n{articles_dict['orientation']}")
+        for article in articles_dict['articles']:
+            f.write(f"\n\nArticle numero {article['id']}")
+            f.write(f"\n{article['titre']}")
+            f.write(f"\nDescription: {article['description']}")
+
+# Recuperer la version textuelle des articles pour le prompt IA
+articles_string = get_articles_string(articles)
+with open("outputs/titres_articles.txt", 'w') as f:
+    f.write(articles_string)
+
+# Recuperer la liste des sujets majeurs avec l'IA
+with open('prompts/prompt_sujets.txt', 'r') as f:
+    prompt_sujets = f.read()
+sujets = prompt_IA("gpt-5.4-mini", prompt_sujets, articles_string)
+sujets_dict = json.loads(sujets)
+print("\nSujets majeurs recuperes.")
+
+# Recuperer la synthese des points de vue pour chaque camp avec l'IA
+articles_descriptions = get_description_articles(articles)
+with open('prompts/prompt_synthese.txt', 'r') as f:
+    prompt_synthese = f.read()
+pdv = get_points_de_vue(sujets_dict, articles_descriptions, prompt_synthese)
+
+with open("outputs/synthese_sujets.txt", 'w') as f:
+    for sujet in sujets_dict['sujets']:
+        nom_sujet = sujet['nom_sujet']
+
+        if nom_sujet in pdv:
+            f.write(f"\n\nSUJET: {nom_sujet}".upper())
+            f.write(f"\n{pdv[nom_sujet]}")
+
+print("\nExecution terminee.")
